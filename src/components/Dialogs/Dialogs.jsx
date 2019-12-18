3c0b5e2014b1db0172f3f784667a58ffef333841
Администратор <v@vvzl.ru>
2019-12-18 22:29:24 +0300

    22 level

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import style from './Dialogs.module.scss';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () =>{
     return(
-        <div className={style.dialogs}>Dialogs</div>
+        <div className={style.dialogs}>
+            <h2 className={style.title}>Dialogs</h2>
+            <div className={style.items}>
+                <div className={style.dialog}>
+                    <NavLink to="dialogs/1">Michael</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="dialogs/2">Lars</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="dialogs/3">Stefan</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="dialogs/4">Angelika</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="dialogs/5">Maria</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>aaaaaaa</div>
+                <div className={style.message}>dddddd</div>
+                <div className={style.message}>ffffff</div>
+                <div className={style.message}>gggggggg</div>
+            </div>
+        </div>
 
     );
 }
